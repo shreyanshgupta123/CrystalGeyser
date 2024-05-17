3d55e88019801798b4779c825dd729972247d1de
shreyanshgupta123 <145015168+shreyanshgupta123@users.noreply.github.com>
2024-05-17 07:46:07 +0530

    17-5-2024

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -7,8 +7,22 @@ import { Router } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-constructor(private route:Router) {
+constructor(private route:Router) {}
+ngAfterViewInit() {
+  const showDrawerBtn = document.getElementById('showDrawerBtn');
+  const hideDrawerBtn = document.querySelector('[data-drawer-hide="drawer-top-example"]');
 
+  // Open drawer when show button is clicked
+  showDrawerBtn?.addEventListener('click', () => {
+    const drawer = document.getElementById('drawer-top-example');
+    drawer?.classList.toggle('-translate-y-full'); // Toggles the visibility of the drawer
+  });
+
+  // Close drawer when hide button is clicked
+  hideDrawerBtn?.addEventListener('click', () => {
+    const drawer = document.getElementById('drawer-top-example');
+    drawer?.classList.add('-translate-y-full'); // Close the drawer by adding the -translate-y-full class
+  });
 }
 navigate(url:string):void{
   if(url=='Products'){
