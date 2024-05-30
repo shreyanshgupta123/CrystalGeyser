0c57b2f44c5fdd50170f1b96ad05c318d4518164
shreyanshgupta123 <145015168+shreyanshgupta123@users.noreply.github.com>
2024-05-30 15:52:32 +0530

    30-05-2024

--- src/app/auth-login/my-wishlist/my-wishlist.component.ts
+++ src/app/auth-login/my-wishlist/my-wishlist.component.ts
@@ -1,28 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../Services/products.service';
 
 @Component({
   selector: 'app-my-wishlist',
   templateUrl: './my-wishlist.component.html',
-  styleUrl: './my-wishlist.component.css'
+  styleUrls: ['./my-wishlist.component.css']
 })
-export class MyWishlistComponent {
-  wishListCount:any
-  constructor(
-    private prod:ProductsService
+export class MyWishlistComponent implements OnInit {
+  wishListCount: number | undefined;
+  prodData: any[] = [];  // Initialize as an array
 
-   )
-  {
-this.prod.getWishList().subscribe(data=>
-  {
-console.log(data)
-this.wishListCount=data
-data.forEach((element:any) => {
-  console.log(element.product_id)
+  constructor(private prod: ProductsService) { }
 
-});
+  ngOnInit(): void {
+    this.prod.getWishList().subscribe(data => {
+      console.log(data);
+      this.wishListCount = data.length;
+      data.forEach((element: any) => {
+        console.log(element.product_id);
+        this.prod.getProductById(element.product_id).subscribe(productData => {
+          console.log(productData);
+          this.prodData.push(productData);  // Push each product to the array
+        });
+      });
+    });
   }
-)
-  }
-
 }
