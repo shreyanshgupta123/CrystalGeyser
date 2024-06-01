24501659efe731456938c5b0b58d4a9f53549fb6
shreyanshgupta123 <145015168+shreyanshgupta123@users.noreply.github.com>
2024-06-01 17:25:12 +0530

    "Refactored MySubscriptionsComponent HTML and TS files: simplified plan status display, removed unnecessary list items, and updated isActive logic."

--- src/app/auth-login/my-subscriptions/my-subscriptions.component.ts
+++ src/app/auth-login/my-subscriptions/my-subscriptions.component.ts
@@ -12,7 +12,6 @@ export class MySubscriptionsComponent {
 subData:any
 subscription:any
 isActive:any
-
   constructor(
     private sub :SubscriptionService,
     private auth:AuthServiceService) {
@@ -25,14 +24,13 @@ this.subscription=data2.subscription
 this.subscription.forEach((element3:any) => {
   if(element3.subscription_category=="ea0e2c6a-a4ce-48a7-bf2c-c05c48d5497a")
     {
-  this.isActive='Active'
-    }else{
-      this.isActive='Cancelled'
+      console.log(this.isActive)
+this.isActive='Active'
+
     }
+    })
+  });
 
-});
-
-  })
 });
       }
     )
